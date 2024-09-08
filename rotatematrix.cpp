65d96254bc1a456dbb2979b18483ea0507be42fe
arjunshetty2003 <120573289+arjunshetty2003@users.noreply.github.com>
2024-09-09 00:01:24 +0530

    Create rotatematrix.cpp

---
+++ rotatematrix.cpp
@@ -0,0 +1,17 @@
+//Matrix Rotate
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) {
+                matrix[i][j] = matrix[i][j]^matrix[j][i];
+                matrix[j][i] = matrix[i][j]^matrix[j][i];
+                matrix[i][j] = matrix[i][j]^matrix[j][i];
+            }
+        }
+        for (int i=0; i<n; i++) {
+            std::reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
