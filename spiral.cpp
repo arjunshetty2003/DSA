2ae97a27f22e4465ca762aefa1d450d2edc025e9
arjunshetty2003 <120573289+arjunshetty2003@users.noreply.github.com>
2024-09-09 23:33:44 +0530

    Create spiral.cpp

---
+++ spiral.cpp
@@ -0,0 +1,36 @@
+//Spiral
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> arr;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int top = 0;
+        int right = n-1;
+        int bottom = m-1;
+        int left = 0;
+        while (top<=bottom && left<=right) {
+           for (int i=left; i<=right; i++) {
+            arr.emplace_back(matrix[top][i]);
+            }
+           top++;
+           for (int i=top; i<=bottom; i++) {
+            arr.emplace_back(matrix[i][right]);
+            }
+           right--;
+           if (top<=bottom) {
+            for (int i=right; i>=left; i--) {
+                arr.emplace_back(matrix[bottom][i]);
+            }
+           }
+            bottom--;
+           if (left<=right) {
+            for (int i=bottom; i>=top; i--) {
+                arr.emplace_back(matrix[i][left]);
+            }
+           }
+           left++;
+        }
+        return arr;
+    }
+};
