b2722731d804a698f996d628c0b7028a75cfea29
Arjun Shetty <arjunshetty0312@gmail.com>
2025-07-31 17:29:13 +0530

    Create rotated_element_ii.cpp

---
+++ rotated_element_ii.cpp
@@ -0,0 +1,18 @@
+//O(n) O(1)
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 2;
+        if (nums.size() <= 2)
+           return nums.size();
+
+        for (int i=2; i< nums.size(); i++) {
+            if (nums[i] != nums[k-2]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
