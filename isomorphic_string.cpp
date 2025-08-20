151ca44aae0ba4da4c83b11c3ec2d045e58af11e
Arjun Shetty <arjunshetty0312@gmail.com>
2025-08-20 12:36:30 +0530

    Create isomorphic_string.cpp

---
+++ isomorphic_string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> s_to_t;
+        unordered_map<char, char> t_to_s;
+
+        for (int i=0; i<s.length(); i++) {
+            char c1 = s[i], c2 = t[i];
+
+            if (s_to_t.count(c1)) {
+                if (c2 != s_to_t[c1]) return false;
+            }
+            else {
+                s_to_t[c1] = c2;
+            }
+
+            if (t_to_s.count(c2)) {
+                if (c1 != t_to_s[c2]) return false;
+            }
+            else {
+                t_to_s[c2] = c1;
+            }
+        }
+
+        return true;
+    }
+};
